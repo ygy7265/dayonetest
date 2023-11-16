b69d5885e9cc43c480d4229a7d845caa5d522b9f
윤경엽(Yun Gyeongyeop) <48234811+ygy7265@users.noreply.github.com>
2023-11-16 18:29:30 +0900

    MYSQLTEST

--- src/test/java/com/ygy/dayonetest/DayonetestApplicationTests.java
+++ src/test/java/com/ygy/dayonetest/DayonetestApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DayonetestApplicationTests {
+class DayonetestApplicationTests extends IntegrationTest{
 
     @Test
     void contextLoads() {
